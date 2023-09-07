2e609ce115d9abb335d621fb68eb26fe6e8ad35b
Yuriy Zaytsev <y.zaytsev@netris.ru>
2023-09-07 17:52:50 +0300

    Немного более правильный поиск трека в файлах

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -2,6 +2,7 @@ package provider
 
 import (
 	"errors"
+	"github.com/RacoonMediaServer/rms-media-discovery/pkg/heuristic"
 	"io/fs"
 	"os"
 	"path/filepath"
@@ -24,14 +25,15 @@ func NewContentProvider(directory string) ContentProvider {
 
 func (p contentProvider) FindTrack(basePath, trackName string) ([]byte, error) {
 	absolutePath := filepath.Join(p.directory, basePath)
-	trackName = strings.ToLower(trackName)
+	trackName = heuristic.Normalize(trackName)
 	found := ""
 
 	err := filepath.WalkDir(absolutePath, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return nil
 		}
-		if strings.Index(strings.ToLower(d.Name()), trackName) >= 0 {
+		normalized := heuristic.Normalize(d.Name())
+		if strings.Index(normalized, trackName) >= 0 {
 			found = path
 			return filepath.SkipAll
 		}
