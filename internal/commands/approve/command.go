81f505994e4cf1bc0c69779f4612c0905f0fb2a8
Yuriy Zaytsev <y.zaytsev@netris.ru>
2023-10-04 19:07:49 +0300

    В теории сделано одобрение заявки

--- internal/commands/approve/command.go
+++ internal/commands/approve/command.go
@@ -4,6 +4,8 @@ import (
 	"github.com/RacoonMediaServer/rms-music-bot/internal/command"
 	"github.com/RacoonMediaServer/rms-music-bot/internal/connectivity"
 	"github.com/RacoonMediaServer/rms-music-bot/internal/messaging"
+	"github.com/RacoonMediaServer/rms-music-bot/internal/registry"
+	rms_users "github.com/RacoonMediaServer/rms-packages/pkg/service/rms-users"
 	"go-micro.dev/v4/logger"
 )
 
@@ -32,5 +34,31 @@ func New(interlayer connectivity.Interlayer, l logger.Logger) command.Command {
 }
 
 func (c approveCommand) Do(ctx command.Context) []messaging.ChatMessage {
+	if len(ctx.Arguments) != 1 {
+		return messaging.NewSingleMessage(command.ParseArgumentsFailed, ctx.ReplyID)
+	}
+	req, ok := registry.Get[*command.Request](c.interlayer.Registry, ctx.Arguments[0])
+	if !ok {
+		return messaging.NewSingleMessage("Заявка не найдена, либо просрочена", ctx.ReplyID)
+	}
+
+	userID := int32(ctx.UserID)
+	user := &rms_users.User{
+		Name:           req.UserName,
+		TelegramUserID: &userID,
+		Perms: []rms_users.Permissions{
+			rms_users.Permissions_Search,
+			rms_users.Permissions_ListeningMusic,
+		},
+	}
+
+	_, err := c.interlayer.Services.NewUsers().RegisterUser(ctx.Ctx, user)
+	if err != nil {
+		c.l.Logf(logger.ErrorLevel, "Register user failed: %s", err)
+	}
+
+	ctx.Chatting.SendTo(req.UserID, messaging.New("Заявка на доступ к боту одобрена! Для справки по командам можно использовать /help.", 0))
+
+	//TODO: register service user
 	return messaging.NewSingleMessage("Заявка одобрена", ctx.ReplyID)
 }
